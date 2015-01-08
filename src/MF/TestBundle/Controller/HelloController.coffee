d3d1d56c067844fde0b58dce5be26e5ac5d06bad
mfrachet <mfrachet@id2tel.com>
2015-01-08 15:32:19 +0100

    Fixed start.js

--- src/MF/TestBundle/Controller/HelloController.coffee
+++ src/MF/TestBundle/Controller/HelloController.coffee
@@ -2,7 +2,6 @@ Controller = use('@Konsserto/Bundle/FrameworkBundle/Controller/Controller')
 
 class HelloController extends Controller
 
-
   indexAction: (name) =>
     @render('MFTestBundle:Hello:index.html.twig', {name: name})
 
--- start.js
+++ start.js
@@ -1,6 +1,13 @@
 var Kernel = require('konsserto');
 var k = new Kernel;
-k.start(require('./app/config/config').port);
+
+var port = 3000;
+config = require('./app/config/config');
+if (config.port) {
+    port = config.port;
+}
+
+k.start(port);
 process.on('SIGINT', function () {
-	k.shutdown(0)
+    k.shutdown(0);
 });
